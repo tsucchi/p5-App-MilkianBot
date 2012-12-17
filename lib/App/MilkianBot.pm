87afedb956d4e778a2fa58311bd7e1ef618a4ab6
Takuya Tsuchida <tsucchi@cpan.org>
2012-12-17 14:10:15 +0900

    initial commit

---
+++ lib/App/MilkianBot.pm
@@ -0,0 +1,32 @@
+package App::MilkianBot;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::MilkianBot -
+
+=head1 SYNOPSIS
+
+  use App::MilkianBot;
+
+=head1 DESCRIPTION
+
+App::MilkianBot is
+
+=head1 AUTHOR
+
+takuya.tsuchida E<lt>tsucchi {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
