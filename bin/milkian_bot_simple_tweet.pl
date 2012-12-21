1fe002fc6565d0fc8784ca2a242101d5d874a5e9
Takuya Tsuchida <tsucchi@cpan.org>
2012-12-21 14:27:23 +0900

    simple_tweet をオブジェクト化

--- bin/milkian_bot_simple_tweet.pl
+++ bin/milkian_bot_simple_tweet.pl
@@ -1,38 +1,15 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Config::Pit;
-use AnyEvent::Twitter;
 use utf8;
 use Encode;
+use App::MilkianBot;
 
 binmode STDOUT, ":utf8";
 my $tweet = decode_utf8($ARGV[0]);
 
 die "no message" if ( !defined $tweet );
 
-my $config = pit_get("milkian_bot", require => {
-    consumer_key    => 'consumer_key',
-    consumer_secret => 'consumer_secret',
-    token           => 'token',
-    token_secret    => 'token_secret',
-});
+my $bot = App::MilkianBot->new();
+$bot->simple_tweet($tweet);
 
-my $done = AnyEvent->condvar;
-my $twitty = AnyEvent::Twitter->new(
-    username        => 'milkian_bot',
-    consumer_key    => $config->{consumer_key},
-    consumer_secret => $config->{consumer_secret},
-    token           => $config->{token},
-    token_secret    => $config->{token_secret},
-);
-$done->begin;
-$twitty->post('statuses/update', {
-    status => $tweet,
-}, sub {
-    my ($header, $response, $reason) = @_;
-    $done->end;
-});
-
-
-$done->recv;
--- lib/App/MilkianBot.pm
+++ lib/App/MilkianBot.pm
@@ -1,7 +1,74 @@
 package App::MilkianBot;
 use strict;
 use warnings;
+use Class::Accessor::Lite (
+    ro => ['my_id', '_cred', '_following_ids'],
+);
+use AnyEvent::Twitter;
+use Config::Pit;
+
 our $VERSION = '0.01';
+
+sub new {
+    my ($class) = @_;
+    my $self = {
+        my_id => '962596830', #milkian_bot
+        _following_ids => [
+            '114700374', #'mimori_suzuko',
+            '261196483', #'tokui_sorangley',
+            '244788445', #'mikoiwate_351',
+            '161910608', #'izugyoza',
+            '94825321',  #'milkyholmes',
+        ],
+    };
+    bless $self, $class;
+    $self->_init_credential();
+    return $self;
+}
+
+sub _init_credential {
+    my ($self) = @_;
+    my $config = pit_get("milkian_bot", require => {
+        consumer_key    => 'consumer_key',
+        consumer_secret => 'consumer_secret',
+        token           => 'token',
+        token_secret    => 'token_secret',
+    });
+
+    $self->{_cred} = {
+        username        => 'milkian_bot',
+        consumer_key    => $config->{consumer_key},
+        consumer_secret => $config->{consumer_secret},
+        token           => $config->{token},
+        token_secret    => $config->{token_secret},
+    };
+}
+
+sub credential {
+    my ($self) = @_;
+    return %{ $self->_cred };
+}
+
+sub following_ids {
+    my ($self) = @_;
+    return @{ $self->_following_ids };
+}
+
+# 単発の tweet を投げます
+sub simple_tweet {
+    my ($self, $tweet) = @_;
+
+    my $done = AnyEvent->condvar;
+    my $twitty = AnyEvent::Twitter->new($self->credential);
+    $done->begin;
+    $twitty->post('statuses/update', {
+        status => $tweet,
+    }, sub {
+        my ($header, $response, $reason) = @_;
+        $done->end;
+    });
+    $done->recv;
+}
 
 1;
 __END__
